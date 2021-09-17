9c97c06f380f1f76d2dbc098226a48c3212f293d
A <solardayz@naver.com>
2021-09-17 14:55:42 +0900

    고객 도메인 생성 및 기본 조회(Rest API)

--- src/main/java/com/mea/ex/cs/service/CSService.java
+++ src/main/java/com/mea/ex/cs/service/CSService.java
@@ -4,6 +4,8 @@ import com.mea.ex.cs.domain.Customer;
 import com.mea.ex.cs.repository.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
 
 @Service
 public class CSService {
@@ -11,6 +13,7 @@ public class CSService {
     @Autowired
     private CustomerRepository customerRepository;
 
+    @Transactional(readOnly = true)
     public Customer findById(Long id){
         return customerRepository.getById(id);
     }
