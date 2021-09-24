9d66cb32be079eac1ffe0916fa219b9e2526e9bb
A <solardayz@naver.com>
2021-09-24 14:48:19 +0900

    ExampleMatcher 적용했으나 정확하게 이해 안감

--- src/test/java/com/mea/ex/cs/repository/CustomerRepositoryTest.java
+++ src/test/java/com/mea/ex/cs/repository/CustomerRepositoryTest.java
@@ -4,10 +4,13 @@ import com.mea.ex.cs.domain.Customer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Example;
+import org.springframework.data.domain.ExampleMatcher;
 
 import java.time.LocalDateTime;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
 
 @SpringBootTest
 class CustomerRepositoryTest {
@@ -17,20 +20,27 @@ class CustomerRepositoryTest {
 
     @Test
     void crud(){
+//        Customer customer = new Customer();
+//        customer.setName("테스트2");
+//        customer.setComment("테스트2 코멘트");
+//        customer.setCreateAt(LocalDateTime.now());
+//        customer.setUpdateAt(LocalDateTime.now());
+//
+//        Customer customer1 = Customer.builder()
+//                .name("테스트3")
+//                .comment("테스트3 코멘트")
+//                .createAt(LocalDateTime.now())
+//                .updateAt(LocalDateTime.now())
+//                .build();
+//
+//        customerRepository.save(customer);
+//        customerRepository.save(customer1);
+
         Customer customer = new Customer();
-        customer.setName("테스트");
-        customer.setComment("테스트 코멘트");
-        customer.setCreateAt(LocalDateTime.now());
-        customer.setUpdateAt(LocalDateTime.now());
+        customer.setName("테스");
+        ExampleMatcher matcher = ExampleMatcher.matching().withMatcher("테", contains());
+        Example<Customer> example = Example.of(customer, matcher);
 
-        Customer customer1 = Customer.builder()
-                .name("테스트1")
-                .comment("테스트1 코멘트")
-                .createAt(LocalDateTime.now())
-                .updateAt(LocalDateTime.now())
-                .build();
-
-        customerRepository.save(customer);
-        customerRepository.save(customer1);
+        customerRepository.findAll(example).forEach(System.out::println);
     }
 }
