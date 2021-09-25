c0190b482751c07efe1c85fa89ca6c555bc1cae3
solardayz <solardayz@naver.com>
2021-09-25 21:10:30 +0900

    Repository Interface 메소드 실습 1

--- src/test/java/com/mea/ex/cs/repository/CustomerRepositoryTest.java
+++ src/test/java/com/mea/ex/cs/repository/CustomerRepositoryTest.java
@@ -1,13 +1,19 @@
 package com.mea.ex.cs.repository;
 
 import com.mea.ex.cs.domain.Customer;
+import org.assertj.core.util.Lists;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
+import org.springframework.data.domain.Sort;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.data.domain.ExampleMatcher.GenericPropertyMatchers.contains;
@@ -19,7 +25,83 @@ class CustomerRepositoryTest {
     private CustomerRepository customerRepository;
 
     @Test
+//    @Transactional
+    void findMethod(){
+        //기본 인서트 문
+        //기본 인서트 문을 사용한 이유는 data.sql의 사용법을 잘 모르겠어서다.
+        //인텔리제이 울티메이트 버전이 아니라서 그런 것 같다.
+        basicInsert();
+
+        //정렬 사용
+        List<Customer> customers = customerRepository.findAll(Sort.by(Sort.Direction.DESC, "name"));
+
+        customers.forEach(System.out::println);
+
+        // in 조건 절
+        List<Long> ids = new ArrayList<>();
+        ids.add(1L);
+        ids.add(2L);
+        ids.add(4L);
+        List<Customer> customers1 = customerRepository.findAllById(ids);
+
+        customers1.forEach(System.out::println);
+
+        // in 조건 절 2
+
+        List<Customer> customers2 = customerRepository.findAllById(Lists.newArrayList(1L, 2L));
+
+        customers2.forEach(System.out::println);
+
+        
+        //org.hibernate.LazyInitializationException: could not initialize proxy [com.mea.ex.cs.domain.Customer#1] - no Session
+        //오류가 남
+//        // 오류가 나지 않기 위해서는 Transactional 필요
+//        Customer customer = customerRepository.getOne(5L);
+//
+//        System.out.println(customer);
+
+        //Optional orElse 사용
+        Customer customer = customerRepository.findById(1L).orElse(null);
+        System.out.println(customer);
+
+
+
+    }
+
+    @Test
+    void saveMethod(){
+        basicInsert();
+
+        Customer customer1 = Customer.builder()
+                .name("guna")
+                .comment("USER")
+                .email("guna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .name("puna")
+                .comment("USER")
+                .email("puna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        customerRepository.saveAll(Lists.newArrayList(customer1, customer2));
+
+        List<Customer> customers = customerRepository.findAll();
+
+        customers.forEach(System.out::println);
+        
+
+
+    }
+
+
+    @Test
     void crud(){
+        // 연습
         Customer customer = new Customer();
         customer.setName("테스트1");
         customer.setComment("테스트1 코멘트");
@@ -57,7 +139,9 @@ class CustomerRepositoryTest {
     }
 
     @Test
-    void select(){
+    void queryMethod1(){
+        
+        //쿼리 메소드 연습
         System.out.println("findByName >"+customerRepository.findByName("테스트1"));
         System.out.println("findByEmail >"+customerRepository.findByEmail("aa@aa.com"));
         System.out.println("getByEmail >"+customerRepository.getByEmail("bb@bb.com"));
@@ -69,5 +153,56 @@ class CustomerRepositoryTest {
         System.out.println("findFirst2ByName >"+customerRepository.findFirst2ByName("테스트2"));
         System.out.println("findTop2ByName >"+customerRepository.findTop2ByName("테스"));
         System.out.println("findLast1ByName >"+customerRepository.findLast1ByName("테"));
+    }
+
+    @Test
+    void basicInsert(){
+        Customer customer1 = Customer.builder()
+                .name("juna")
+                .comment("BOSS")
+                .email("juna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        Customer customer2 = Customer.builder()
+                .name("auna")
+                .comment("MANAGER")
+                .email("auna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        Customer customer3 = Customer.builder()
+                .name("euna")
+                .comment("USER")
+                .email("euna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        Customer customer4 = Customer.builder()
+                .name("buna")
+                .comment("USER")
+                .email("buna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        Customer customer5 = Customer.builder()
+                .name("quna")
+                .comment("USER")
+                .email("quna@clc.com")
+                .createAt(LocalDateTime.now())
+                .updateAt(LocalDateTime.now())
+                .build();
+
+        customerRepository.save(customer1);
+        customerRepository.save(customer2);
+        customerRepository.save(customer3);
+        customerRepository.save(customer4);
+        customerRepository.save(customer5);
+
+        //customerRepository.findAll().forEach(System.out::println);
     }
 }
---
+++ src/test/resources/data.sql
@@ -0,0 +1,14 @@
+call next value for hibernate_sequence;
+insert into Customer('id', 'name', 'comment', 'email', 'createAt', 'updateAt') values (1, 'juna', 'boss', 'juna@clc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into Customer('id', 'name', 'comment', 'email', 'createAt', 'updateAt') values (2, 'jina', 'manager', 'jina@clc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into Customer('id', 'name', 'comment', 'email', 'createAt', 'updateAt') values (3, 'muna', 'user', 'muna@clc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into Customer('id', 'name', 'comment', 'email', 'createAt', 'updateAt') values (4, 'yuna', 'user', 'yuna@clc.com', now(), now());
+
+call next value for hibernate_sequence;
+insert into Customer('id', 'name', 'comment', 'email', 'createAt', 'updateAt') values (5, 'kuna', 'user', 'kuna@clc.com', now(), now());
