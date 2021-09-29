58d0a7d9284682a0041fb4bd4fd9b9cce881fd33
A <solardayz@naver.com>
2021-09-29 10:31:37 +0900

    쿼리메소드 2, 3 입력 where 절 변경 및 sort, page

--- src/main/java/com/mea/ex/cs/repository/CustomerRepository.java
+++ src/main/java/com/mea/ex/cs/repository/CustomerRepository.java
@@ -1,8 +1,12 @@
 package com.mea.ex.cs.repository;
 
 import com.mea.ex.cs.domain.Customer;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.time.LocalDateTime;
 import java.util.*;
 
 public interface CustomerRepository extends JpaRepository<Customer, Long> {
@@ -30,4 +34,51 @@ public interface CustomerRepository extends JpaRepository<Customer, Long> {
     List<Customer> findTop2ByName(String name);
 
     List<Customer> findLast1ByName(String name);
+
+    //쿼리 메소드 2
+
+    List<Customer> findByEmailAndName(String email, String name);
+
+    List<Customer> findByEmailOrName(String email, String name);
+
+    List<Customer> findByCreatedAtAfter(LocalDateTime yesterday);
+
+    List<Customer> findByIdAfter(Long id);
+
+    List<Customer> findByCreatedAtGreaterThan(LocalDateTime yesterday);
+
+    List<Customer> findByCreatedAtGreaterThanEqual(LocalDateTime yesterday);
+
+    List<Customer> findByCreatedAtBetween(LocalDateTime yesterday, LocalDateTime tomorrow);
+
+    List<Customer> findByIdBetween(Long id1, Long id2);
+
+    List<Customer> findByIdGreaterThanEqualAndIdLessThanEqual(Long id1, Long id2);
+
+    List<Customer> findByIdIsNotNull();
+
+//    List<User> findByAddressIsNotEmpty();   // name is not null and name != '' ??
+
+    List<Customer> findByNameIn(List<String> names);
+
+    List<Customer> findByNameStartingWith(String name);
+
+    List<Customer> findByNameEndingWith(String name);
+
+    List<Customer> findByNameContains(String name);
+
+    List<Customer> findByNameLike(String name);
+
+
+    //쿼리 메소드 3
+
+    List<Customer> findTop1ByName(String name);
+
+    List<Customer> findTopByNameOrderByIdDesc(String name);
+
+    List<Customer> findFirstByNameOrderByIdDescEmailAsc(String name);
+
+    List<Customer> findFirstByName(String name, Sort sort);
+
+    Page<Customer> findByName(String name, Pageable pageable);
 }
